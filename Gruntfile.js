6da840aa0c402232a396dbba2a5b41178c3d8b15
PProvost <peter@provost.org>
2013-08-19 11:38:22 -0600

    Split source and public and update grunt to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-	
+
 	// Project config
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -9,16 +9,27 @@ module.exports = function(grunt) {
 					yuicompress: true
 				},
 				files: {
-						"public/css/main.css": "less/main.less"
-				}
+					"public/css/main.css": "src/less/main.less"
+				},
+			},
+		},
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['img/**/*.{png,jpg,gif}'],
+					dest: 'public/'
+				}]
 			}
-		}
-  });
+		},
+	});
 
-  // Load the plugins
+	// Load the plugins
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  // Default task(s).
-	grunt.registerTask('default', ['less']);
+	// Default task(s).
+	grunt.registerTask('default', ['less', 'imagemin']);
 
 };
