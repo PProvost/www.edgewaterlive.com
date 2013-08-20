d44dd01bc4467ab36f1fcfa792dc9c6e2ffb2cc6
PProvost <peter@provost.org>
2013-08-19 22:52:20 -0600

    Added htmlmin to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,11 +27,22 @@ module.exports = function(grunt) {
 		copy: {
 			main: {
 				files: [
-					{expand: true, cwd: 'src/', src: ['*.html'], dest: 'public/', filter: 'isFile'},
 					{expand: true, cwd: 'src/ico', src: ['*'], dest: 'public/ico/', filter: 'isFile'},
 					{expand: true, cwd: 'src/js', src: ['*.js'], dest: 'public/js/', filter: 'isFile'},
 				]
 			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					collapseWhitespace: true,
+				},
+				files: {
+					'public/index.html': 'src/index.html',
+					'public/404.html': 'src/404.html',
+				},
+			},
 		},
 	});
 
@@ -40,8 +51,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
 	// Default task(s).
-	grunt.registerTask('default', ['clean', 'copy', 'less', 'imagemin']);
+	grunt.registerTask('default', ['clean', 'htmlmin', 'copy', 'less', 'imagemin']);
 
 };
