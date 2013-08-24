5f0abf15cd94d4e2f87e0ffcb3fbf683f3ccd202
PProvost <peter@provost.org>
2013-08-23 22:52:33 -0600

    Added sitemap task and result

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,36 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
+	// Custom sitemap task since the plugin on grunt.js is broken
+	grunt.registerTask('sitemap', 'Creates a sitemap for all html files in public/', function() {
+		// Options
+		var rootUrl = 'http://www.edgewaterliveband.com/';
+		var basePath = 'public';
+		var changefreq = 'daily';
+		
+		// Guts
+		var fs = require('fs');
+		var lf = grunt.util.linefeed;
+		var files = grunt.file.expand({ cwd: basePath }, '**/*.html');
+		var xml = '<?xml version="1.0" encoding="UTF-8"?>' + lf;
+		xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' + lf;
+		for (var index in files) {
+			var file = files[index];
+			if (!file.match(/404\.html$/i)) {
+				xml += '<url>' + lf;
+				xml += '<loc>' + rootUrl + file + '</loc>' + lf;
+				var mtime = (fs.statSync(basePath + '/' + file).mtime).getTime() 
+				xml += '<lastmod>' + (new Date(mtime).toISOString()) + '</lastmod>';
+				xml += '<changefreq>' + changefreq + '</changefreq>';
+				xml += '</url>' + lf;
+			}
+		};
+		xml += '</urlset>';
+		grunt.file.write(basePath + '/sitemap.xml', xml);
+		grunt.log.writeln("Sitemap.xml created successfully");	
+	});
+
 	// Default task(s).
-	grunt.registerTask('default', ['clean', 'htmlmin', 'copy', 'less', 'imagemin']);
+	grunt.registerTask('default', ['clean', 'htmlmin', 'copy', 'less', 'imagemin', 'sitemap']);
 
 };
