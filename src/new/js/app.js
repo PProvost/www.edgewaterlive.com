8874f424ea95b59351f328adfbbfe1141537128c
PProvost <peter@provost.org>
2014-02-22 00:45:15 -0700

    Almost done with the new one

--- src/new/js/app.js
+++ src/new/js/app.js
@@ -1,1 +1,10 @@
+function scrollIt(elt) {
+	var offset = $(elt).offset();
+	offset.top -= 60;
+	$('html, body').animate({
+		scrollTop: offset.top,
+		scrollLeft: offset.left
+	});
+}
+
 $(document).foundation();
