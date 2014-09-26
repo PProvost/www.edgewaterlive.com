fec5434fd94b51d638cf29196a8ef5ff9276916e
PProvost <peter@provost.org>
2014-09-26 10:20:17 -0600

    Removed unused grunt-shell task

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,7 +133,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-shell');
 	grunt.loadNpmTasks('grunt-convert');
 	grunt.loadNpmTasks('assemble');
 
